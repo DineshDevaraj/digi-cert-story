4901b4e63fdd408fcb5d929eaca215c9a7c74868
dinesh.devaraj <ddinesh.software@gmail.com>
2025-07-29 11:53:54 +0000

    fix style errors

--- main.py
+++ main.py
@@ -9,25 +9,37 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request, page: int = Cookie(None), 
-                    scenario: str = Cookie(None), approach: str = Cookie(None)):
+async def read_root(
+    request: Request,
+    page: int = Cookie(None),
+    scenario: str = Cookie(None),
+    approach: str = Cookie(None),
+):
     if page is None:
         response = templates.TemplateResponse("page1.html", {"request": request})
         response.set_cookie(key="page", value=1)
-        return response 
+        return response
 
-    return templates.TemplateResponse(f"page{page}.html", {
-        "request": request, "scenario": scenario, "approach": approach})
+    return templates.TemplateResponse(
+        f"page{page}.html",
+        {"request": request, "scenario": scenario, "approach": approach},
+    )
 
 
 @app.post("/next-page", response_class=HTMLResponse)
-async def goto_next_page(request: Request, page: int = Cookie(None),
-                      scenario: str = Form(...), approach: str = Form(...)):
-    response = templates.TemplateResponse("result.html", {
-        "request": request, "scenario": scenario, "approach": approach})
+async def goto_next_page(
+    request: Request,
+    page: int = Cookie(None),
+    scenario: str = Form(...),
+    approach: str = Form(...),
+):
+    response = templates.TemplateResponse(
+        "result.html", {"request": request, "scenario": scenario, "approach": approach}
+    )
     response.set_cookie(key="scenario", value=scenario)
     response.set_cookie(key="approach", value=approach)
-    response.set_cookie(key="page", value=page+1)
+    response.set_cookie(key="page", value=page + 1)
     return response
+
 
 # uvicorn main:app --reload --host 0.0.0.0 --port 7071
