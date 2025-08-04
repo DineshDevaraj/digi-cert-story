9b75a9e142a2595cfbb259f0decc94449d976223
dinesh.devaraj <ddinesh.software@gmail.com>
2025-08-04 10:15:39 +0000

    sequence diagram URL to the top

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ templates = Jinja2Templates(directory="templates")
 # Mount static files if needed
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-track_folder = {}
+tracks = {}
 scenario = ["Validate Website", "Network Management System"]
 approach = ["Theoretical", "Command Line Interface", "Python"]
 for i, s in enumerate(scenario):
     for j, a in enumerate(approach):
-        track_folder[(s, a)] = f"track{i+1}{j+1}"
+        tracks[(s, a)] = f"track{i+1}{j+1}"
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -33,7 +33,7 @@ async def read_root(
         response.set_cookie(key="page", value=page)
         return response
 
-    track = track_folder[(scenario, approach)]
+    track = tracks[(scenario, approach)]
     return templates.TemplateResponse(
         f"{track}/page{page}.html",
         {"request": request, "page": page, "scenario": scenario, "approach": approach, "track": track},
@@ -79,6 +79,20 @@ async def restart(
     response.set_cookie(key="page", value=2)
     response.delete_cookie(key="scenario")
     response.delete_cookie(key="approach")
+    response.status_code = status.HTTP_200_OK
+    return response
+
+
+@app.get("/sequence_diagram", response_class=HTMLResponse)
+async def sequence_diagram(
+    request: Request,
+    scenario: Optional[str] = Cookie(...),
+    approach: Optional[str] = Cookie(...),
+):
+    track = tracks[(scenario, approach)]
+    response = templates.TemplateResponse(
+        "sequence_diagram.html", {"request": request, "scenario": scenario, "approach": approach, "track": track}
+    )
     response.status_code = status.HTTP_200_OK
     return response
 
